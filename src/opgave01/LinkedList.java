30b75a9266a8009d0aa35b1a66a94fb3e8d130f6
DIDER <Simon.dider@gmail.com>
2025-04-01 16:50:27 +0200

    opgave

---
+++ src/opgave01/LinkedList.java
@@ -0,0 +1,77 @@
+package opgave01;
+
+import opgave01.models.ListEaaa;
+
+import java.util.Iterator;
+
+public class LinkedList implements ListEaaa {
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public void addFirst(Object o) {
+
+    }
+
+    @Override
+    public Object getFirst() {
+        return null;
+    }
+
+    @Override
+    public void removeFirst() {
+
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object o) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
